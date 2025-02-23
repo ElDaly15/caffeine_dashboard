db9029e35f7ab13923deed762d8322bfbd95c081
Daly <dalydevelopment15@gmail.com>
2025-02-23 16:19:57 +0200

    Add new fonts, images, and initial home view widget

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color mainColorTheme = Color(0xffC67C4E);
+  static const Color secondaryColorTheme = Color(0xffEDD6C8);
+  static const Color darkTheme = Color(0xff313131);
+  static const Color whiteColorFirst = Color(0xffE3E3E3);
+  static const Color whiteColorSecond = Color(0xffF9F2ED);
+}

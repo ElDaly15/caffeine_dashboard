cc19ba0f0578179ed12b39b0bc19efccecf77e5d
Daly <dalydevelopment15@gmail.com>
2025-04-05 17:45:00 +0200

    refactor: Improve EditNotificationViewBody layout and text field handling

--- lib/featuers/notification/presentation/views/widgets/edit_notfication_view_body.dart
+++ lib/featuers/notification/presentation/views/widgets/edit_notfication_view_body.dart
@@ -20,93 +20,95 @@ class _EditNotficationViewBodyState extends State<EditNotficationViewBody> {
   String? titleEn, titleAr, bodyEn, bodyAr;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 22),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(height: 20),
-          Text('Edit Title [En]', style: TextStyles.font20SemiBold(context)),
-          CustomEditTextField(
-            maxlines: 1,
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 22),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 20),
+            Text('Edit Title [En]', style: TextStyles.font20SemiBold(context)),
+            CustomEditTextField(
+              maxlines: 1,
 
-            title: 'Title [En]',
-            onChanged: (value) {
-              titleEn = value;
-            },
-            textEditingController: TextEditingController(
-              text: titleEn ?? widget.notificationModel.titleEn,
+              title: 'Title [En]',
+              onChanged: (value) {
+                titleEn = value;
+              },
+              textEditingController: TextEditingController(
+                text: titleEn ?? widget.notificationModel.titleEn,
+              ),
+              textInputType: TextInputType.text,
             ),
-            textInputType: TextInputType.text,
-          ),
-          const SizedBox(height: 20),
-          Text('Edit Title [Ar]', style: TextStyles.font20SemiBold(context)),
-          CustomEditTextField(
-            maxlines: 1,
+            const SizedBox(height: 20),
+            Text('Edit Title [Ar]', style: TextStyles.font20SemiBold(context)),
+            CustomEditTextField(
+              maxlines: 1,
 
-            title: 'Title [Ar]',
-            onChanged: (value) {
-              titleAr = value;
-            },
-            textEditingController: TextEditingController(
-              text: titleAr ?? widget.notificationModel.titleAr,
+              title: 'Title [Ar]',
+              onChanged: (value) {
+                titleAr = value;
+              },
+              textEditingController: TextEditingController(
+                text: titleAr ?? widget.notificationModel.titleAr,
+              ),
+              textInputType: TextInputType.text,
             ),
-            textInputType: TextInputType.text,
-          ),
-          const SizedBox(height: 20),
-          Text(
-            'Edit Description [En]',
-            style: TextStyles.font20SemiBold(context),
-          ),
-          CustomEditTextField(
-            maxlines: 5,
-
-            title: 'Description [En]',
-            onChanged: (value) {
-              bodyEn = value;
-            },
-            textEditingController: TextEditingController(
-              text: bodyEn ?? widget.notificationModel.bodyEn,
+            const SizedBox(height: 20),
+            Text(
+              'Edit Description [En]',
+              style: TextStyles.font20SemiBold(context),
             ),
-            textInputType: TextInputType.text,
-          ),
-          const SizedBox(height: 20),
-          Text(
-            'Edit Description [Ar]',
-            style: TextStyles.font20SemiBold(context),
-          ),
-          CustomEditTextField(
-            maxlines: 5,
+            CustomEditTextField(
+              maxlines: 5,
 
-            title: 'Description [Ar]',
-            onChanged: (value) {
-              bodyAr = value;
-            },
-            textEditingController: TextEditingController(
-              text: bodyAr ?? widget.notificationModel.bodyAr,
+              title: 'Description [En]',
+              onChanged: (value) {
+                bodyEn = value;
+              },
+              textEditingController: TextEditingController(
+                text: bodyEn ?? widget.notificationModel.bodyEn,
+              ),
+              textInputType: TextInputType.text,
             ),
-            textInputType: TextInputType.text,
-          ),
-          const SizedBox(height: 20),
-          CustomBigElevatedBtmWithIcon(
-            onPressed: () {
-              widget.notificationModel.titleEn =
-                  titleEn ?? widget.notificationModel.titleEn;
-              widget.notificationModel.titleAr =
-                  titleAr ?? widget.notificationModel.titleAr;
-              widget.notificationModel.bodyEn =
-                  bodyEn ?? widget.notificationModel.bodyEn;
-              widget.notificationModel.bodyAr =
-                  bodyAr ?? widget.notificationModel.bodyAr;
+            const SizedBox(height: 20),
+            Text(
+              'Edit Description [Ar]',
+              style: TextStyles.font20SemiBold(context),
+            ),
+            CustomEditTextField(
+              maxlines: 5,
 
-              BlocProvider.of<ManageNotificationCubit>(
-                context,
-              ).updateNotification(notification: widget.notificationModel);
-            },
-            title: 'Save',
-            iconData: FontAwesomeIcons.floppyDisk,
-          ),
-        ],
+              title: 'Description [Ar]',
+              onChanged: (value) {
+                bodyAr = value;
+              },
+              textEditingController: TextEditingController(
+                text: bodyAr ?? widget.notificationModel.bodyAr,
+              ),
+              textInputType: TextInputType.text,
+            ),
+            const SizedBox(height: 20),
+            CustomBigElevatedBtmWithIcon(
+              onPressed: () {
+                widget.notificationModel.titleEn =
+                    titleEn ?? widget.notificationModel.titleEn;
+                widget.notificationModel.titleAr =
+                    titleAr ?? widget.notificationModel.titleAr;
+                widget.notificationModel.bodyEn =
+                    bodyEn ?? widget.notificationModel.bodyEn;
+                widget.notificationModel.bodyAr =
+                    bodyAr ?? widget.notificationModel.bodyAr;
+
+                BlocProvider.of<ManageNotificationCubit>(
+                  context,
+                ).updateNotification(notification: widget.notificationModel);
+              },
+              title: 'Save',
+              iconData: FontAwesomeIcons.floppyDisk,
+            ),
+          ],
+        ),
       ),
     );
   }
